be8d77d411b15efc7cdff9ea3a6959dbd56ed7ef
Michael Dunlap <sagaquisces@gmail.com>
2017-08-18 14:43:02 -0700

    put add animal form in a panel.

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -4,51 +4,80 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'new-animal',
   template: `
-    <h1>New Animal</h1>
-    <hr>
     <div class="row">
-      <div class="col-md-4">
-        <div class="form-group">
-          <label class="label label-primary" for="species">Species:</label>
-          <input class="form-control" id="species" #newSpecies />
-        </div>
-        <div class="form-group">
-          <label class="label label-primary" for="name">Name:</label>
-          <input class="form-control" id="age" #newName />
-        </div>
-        <div class="row">
-          <div class="col-xs-4">
-            <div class="form-group">
-              <label class="label label-primary" for="age">Age:</label>
-              <input class="form-control" id="age" type="number" #newAge />
-            </div>
+      <div class="col-md-6">
+
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h3>New Animal</h3>
           </div>
-          <div class="col-xs-8">
+          <div class="panel-body">
+
             <div class="form-group">
-              <label class="label label-primary" for="diet">Diet:</label>
-              <select class="form-control" id="diet" #newDiet>
-                <option [value]="Carnivore">Carnivore</option>
-                <option [value]="Herbivore">Herbivore</option>
-                <option [value]="Omnivore">Omnivore</option>
-              </select>
+              <label class="label label-primary" for="species">Species:</label>
+              <input class="form-control" id="species" #newSpecies />
             </div>
+            <div class="form-group">
+              <label class="label label-primary" for="name">Name:</label>
+              <input class="form-control" id="age" #newName />
+            </div>
+
+            <div class="row">
+              <div class="col-xs-3">
+                <div class="form-group">
+                  <label class="label label-primary" for="age">Age:</label>
+                  <input class="form-control" id="age" type="number" #newAge />
+                </div>
+              </div>
+              <div class="col-xs-3">
+                <div class="form-group">
+                  <label class="label label-primary" for="sex">Sex:</label>
+                  <select class="form-control" id="sex" #newSex>
+                    <option [value]="Male">Male</option>
+                    <option [value]="Female">Female</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-xs-6">
+                <div class="form-group">
+                  <label class="label label-primary" for="diet">Diet:</label>
+                  <select class="form-control" id="diet" #newDiet>
+                    <option [value]="Carnivore">Carnivore</option>
+                    <option [value]="Herbivore">Herbivore</option>
+                    <option [value]="Omnivore">Omnivore</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-8">
+                <div class="form-group">
+                  <label class="label label-primary" for="location">Location:</label>
+                  <input class="form-control" id="location" #newLocation />
+                </div>
+              </div>
+              <div class="col-xs-4">
+                <div class="form-group">
+                  <label class="label label-primary" for="caretakers">No. of Caretakers:</label>
+                  <input class="form-control" id="caretakers" type="number" #newCaretakers />
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="label label-primary" for="likes">Likes:</label>
+              <input class="form-control" id="likes" #newLikes />
+            </div>
+            <div class="form-group">
+              <label class="label label-primary" for="dislikes">Dislikes:</label>
+              <input class="form-control" id="dislikes" #newDislikes />
+            </div>
+            <button class="btn"(click)="doneButtonClicked()">Done</button>
+
+
           </div>
         </div>
-        <div class="row">
-          <div class="col-xs-8">
-            <div class="form-group">
-              <label class="label label-primary" for="location">Location:</label>
-              <input class="form-control" id="location" #newLocation />
-            </div>
-          </div>
-          <div class="col-xs-4">
-            <div class="form-group">
-              <label class="label label-primary" for="caretakers">No. of Caretakers:</label>
-              <input class="form-control" id="caretakers" type="number" #newCaretakers />
-            </div>
-          </div>
-        </div>
-        <button class="btn btn-default"(click)="doneButtonClicked()">Done</button>
+
       </div>
     </div>
   `
